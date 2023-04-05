acf2aca1afe25f352ffad8f9bf67c5048de5bc31
K.Sivabalan <ksivabalan.91@gmail.com>
2023-04-05 12:44:43 +0800

    completed task 8

--- src/main/java/ibf2022/paf/assessment/server/controllers/TasksController.java
+++ src/main/java/ibf2022/paf/assessment/server/controllers/TasksController.java
@@ -1,16 +1,16 @@
 package ibf2022.paf.assessment.server.controllers;
 
-import java.sql.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import ibf2022.paf.assessment.server.services.TodoService;
 
@@ -27,17 +27,19 @@ public class TasksController {
     // {username=[fred], description-0=[item 1], priority-0=[1], dueDate-0=[2023-04-12], description-1=[item 2], priority-1=[2], dueDate-1=[2023-04-19]}"
 
     @PostMapping(path="/task", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public ResponseEntity<String> postTask(@RequestBody MultiValueMap<String,String> form){
-        logger.info("Starting logger");
-
-        // String date = form.getFirst("dueDate-0");
-
+    public ModelAndView postTask(@RequestBody MultiValueMap<String,String> form, Model model){
+        logger.info(">>> Running upsertTask service");
         int rows = todoSvc.upsertTask(form);
+        logger.info(">>> upsertTask complete, %d rows inserted".formatted(rows));
+
+        // model.addAttribute("taskCount", rows);
+        // model.addAttribute("username", form.getFirst("username"));
+
+        ModelAndView mav = new ModelAndView("result");
+        mav.addObject("taskCount", rows);
+        mav.addObject("username", form.getFirst("username"));
+        mav.setStatus(HttpStatusCode.valueOf(200));
         
-
-        return ResponseEntity.ok().body("%d rows inserted".formatted(rows));
+        return mav;
     }
-    
-
-    
 }
